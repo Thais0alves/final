fbc25f1f9122db7547206f6776c22cbbbed81275
Thais0alves <00001096492672sp@al.educacao.sp.gov.br>
2024-09-20 11:06:54 -0300

    Add files via upload

---
+++ script (2) (2).js
@@ -0,0 +1,11 @@
+const avança = document.querySelectorAll(btn-proximo)
+
+    avança. forEach(button = {
+button  addEventListener(click function()
+    const atual= document.querySelector(ativo)
+const proximo passo = passo + this.getAttribute("data-proximo")
+    
+atual.classlist.remove(ativo);
+document.getElementById(proximo passo).classlist.add(ativo);
+})  
+
